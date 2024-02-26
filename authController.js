0acc4acf6426cb661d0767368a955854edd99af1
dendywool <zaharkhachaturyan@gmail.com>
2024-02-26 14:58:15 +0100

    registration realized

--- authController.js
+++ authController.js
@@ -1,9 +1,36 @@
+const Role = require("./models/Role");
+const User = require("./models/User");
+const bcrypt = require('bcryptjs');
+const { validationResult } = require('express-validator');
+
 class authController {
     async registration(req, res) {
         try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ message: "Error with registration", errors })
+            }
+            const { username, password } = req.body;
+            const candidate = await User.findOne({ username });
 
+            if (candidate) {
+                return res.status(400).json({ message: "There is one user with same name" });
+            }
+            const hashUserPassword = bcrypt.hashSync(password, 8);
+            const userRole = await Role.findOne({ value: 'USER' });
+
+            if (!Role) {
+                return res.status(400).json({ message: "Role 'USER' not found" });
+            }
+
+            const user = new User({ username, password: hashUserPassword, roles: [userRole.value] });
+
+            await user.save();
+
+            return res.json({ message: "User was successfully created" })
         } catch (error) {
-
+            console.log(error);
+            res.status(400).json({ message: "Registration error" })
         }
     }
 
@@ -11,7 +38,8 @@ class authController {
         try {
 
         } catch (error) {
-
+            console.log(error);
+            res.status(400).json({ message: "Login error" })
         }
     }
 
--- authRoute.js
+++ authRoute.js
@@ -1,10 +1,13 @@
 const Router = require('express');
 const authController = require("./authController");
-
+const { check } = require('express-validator');
 
 const router = new Router();
 
-router.post('/registration', authController.registration);
+router.post('/registration', [
+    check('username', "Name cannot be empty").notEmpty(),
+    check('password', "Password must have more than 5 symbols").isLength({ min: 5, max: 10 })
+], authController.registration);
 router.post('/login', authController.login);
 router.get('/users', authController.getUsers);
 
