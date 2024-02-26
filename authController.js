3263b02722f80d1e7ddb7e4236b8072377a8153a
dendywool <zaharkhachaturyan@gmail.com>
2024-02-26 13:58:54 +0100

    realized controlle

---
+++ authController.js
@@ -0,0 +1,26 @@
+class authController {
+    async registration(req, res) {
+        try {
+
+        } catch (error) {
+
+        }
+    }
+
+    async login(req, res) {
+        try {
+
+        } catch (error) {
+
+        }
+    }
+
+    async getUsers(req, res) {
+        try {
+            res.json("server work")
+        } catch (error) {
+
+        }
+    }
+}
+module.exports = new authController();
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const authRouter = require("./authRoute");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(express.json());
+app.use('/auth', authRouter);
 
 const start = async () => {
     try {
